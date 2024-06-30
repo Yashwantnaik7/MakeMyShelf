d7a77adfb0326cdaa792b6e2f78fcca731d781bd
Yashwantnaik7 <yashnaik322@gmail.com>
2024-06-30 11:00:49 +0530

    created color and image constants

---
+++ src/constants/Colors.constant.ts
@@ -0,0 +1,9 @@
+const Colors = {
+    black: '#0000',
+    white: '#ffff',
+    blue: '#0373F3',
+    gray: '#BCBCBC'
+
+}
+
+export default Colors
---
+++ src/constants/Images.constant.ts
@@ -0,0 +1,14 @@
+const Images = {
+    tabbar: {
+        selectedHome: require('../assets/images/home_selected.png'),
+        unSelectedHome: require('../assets/images/home_unselected.png'),
+        selectedWallet: require('../assets/images/wallet_selected.png'),
+        unSelectedWallet: require('../assets/images/wallet_unselected.png'),
+        selectedGuide: require('../assets/images/guide_selected.png'),
+        unSelectedGuide: require('../assets/images/guide_unselected.png'),
+        selectedChart: require('../assets/images/chart_selected.png'),
+        unSelectedChart: require('../assets/images/chart_unselected.png'),
+    }
+}
+
+export default Images
